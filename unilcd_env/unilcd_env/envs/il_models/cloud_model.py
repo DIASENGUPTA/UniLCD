e756b5c5ae94519a821233f2f3908c374661931f
Kathakoli Sengupta <senguptadia25@gmail.com>
2024-08-25 21:01:37 -0400

    Update envs

---
+++ unilcd_env/unilcd_env/envs/il_models/cloud_model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+# # Create a new model without these layers
+class CloudModel(nn.Module):
+    def __init__(self):
+        super(CloudModel, self).__init__()
+        # Add your model class for use
+        pass
+    
+    def forward(self, x,locations):
+        # Add the forward pass for your model below
+        pass
